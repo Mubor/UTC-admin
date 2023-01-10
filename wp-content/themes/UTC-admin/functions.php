9694c9708f4569a254c6001c96e41641e42c1648
K0Nz1dd <k0nz1dd@gmail.com>
2023-01-10 19:48:03 +0200

    add pages func

--- wp-content/themes/UTC-admin/functions.php
+++ wp-content/themes/UTC-admin/functions.php
@@ -1,24 +1,26 @@
 <?php
 
-add_action('wp_enqueue_scripts', 'main_page', 10);
-add_action('wp_enqueue_scripts', 'contacts', 10);
+add_action('wp_enqueue_scripts', 'main_page');
+add_action('wp_enqueue_scripts', 'contacts_page');
 // add_action('wp_enqueue_scripts', 'test');
 show_admin_bar(true);
 
 
 function main_page() {
-    // if(is_page('test1') ){
+    if(is_page('home') ){
         wp_enqueue_style( 'style', get_template_directory_uri() . '/assets/css/home/style.css', 'all');
         wp_enqueue_style( 'footer', get_template_directory_uri() . '/assets/styles/footer/footer.css', 'all');
         wp_enqueue_script('home', get_template_directory_uri() . '/assets/dist/home.js',array(),'2023', true);        
-    // }
+    }
 }
-function contacts() {
-    // if(is_page('test1') ){
+function contacts_page() {
+    if(is_page('contacts') ){
         wp_enqueue_style( 'style', get_template_directory_uri() . '/assets/css/contacts/style.css', 'all');
         wp_enqueue_style( 'footer', get_template_directory_uri() . '/assets/styles/footer/footer.css', 'all');
         wp_enqueue_script('contacts', get_template_directory_uri() . '/assets/dist/contacts.js',array(),'2023', true);        
-    // }
+    }
 }
+
+
 
 ?>
